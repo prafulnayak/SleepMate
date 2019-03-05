d303e77d93a29b4fd5b2cb81e87380daf451cc6f
Praful Nayak <prafulnayak1988@gmail.com>
2019-03-06 01:26:52 +0530

    Interval changed

--- app/src/main/java/com/anyemi/sleepmate/Services/ServiceUtils.java
+++ app/src/main/java/com/anyemi/sleepmate/Services/ServiceUtils.java
@@ -14,7 +14,7 @@ import com.firebase.jobdispatcher.Trigger;
 import java.util.concurrent.TimeUnit;
 
 public class ServiceUtils {
-    private static final int INTERVAL_MINUTES = 2;
+    private static final int INTERVAL_MINUTES = 1;
     private static final int INTERVAL_SECONDS = (int) (TimeUnit.MINUTES.toSeconds(INTERVAL_MINUTES));
     private static final int SYNC_FLEX_TIMEWINDOW = INTERVAL_SECONDS;
     private static final String RTAG = "reminder_tag";
